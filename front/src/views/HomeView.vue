4f0a45f9daa535f600f5d96e8f30c1d1c9b690b6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-30 14:56:14 +0200

    dev: added button css

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -2,6 +2,19 @@
 
 <template>
   <main>
-    <p>Home</p>
+    <h1>Gérer efficacement votre stock</h1>
+    <RouterLink to="/stock" class="button primary">Voir le stock</RouterLink>
+    <button class="button primary">Voir le stock</button>
+    <RouterLink to="/stock" class="button">Voir le stock</RouterLink>
+    <button href="dd" class="button" disabled>Voir le stock</button>
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: column;
+}
+</style>

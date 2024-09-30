8dd2df8ac25aaedf2298faa07313e748d53c6f47
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-30 16:16:32 +0200

    dev: added table decoration

--- front/src/stock/views/ListView.vue
+++ front/src/stock/views/ListView.vue
@@ -3,7 +3,56 @@
 <template>
   <main>
     <h1>Liste des articles</h1>
+    <div class="content">
+      <nav>
+        <button>Rafraîchir</button>
+        <RouterLink to="add" class="button">Ajouter</RouterLink>
+        <button>Supprimer</button>
+      </nav>
+      <table>
+        <thead>
+          <tr>
+            <th class="name">Nom</th>
+            <th class="price">Prix</th>
+            <th class="qty">Quantité</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="name">Tournevis</td>
+            <td class="price">3.45 €</td>
+            <td class="qty">123</td>
+          </tr>
+          <tr>
+            <td class="name">Pelle</td>
+            <td class="price">13.45 €</td>
+            <td class="qty">6</td>
+          </tr>
+          <tr class="selected">
+            <td class="name">Arrosoir</td>
+            <td class="price">5.00 €</td>
+            <td class="qty">12</td>
+          </tr>
+          <tr>
+            <td class="name">Marteau</td>
+            <td class="price">34.00 €</td>
+            <td class="qty">8</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+div.content {
+  display: flex;
+  flex-flow: column;
+  gap: 3em;
+
+  nav {
+    display: flex;
+    gap: 0.25rem;
+  }
+}
+</style>

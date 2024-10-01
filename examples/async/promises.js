e7fb10be01fa5ad525e785f9872a0e2e47b4405c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-01 14:05:00 +0200

    dev: promise from fs

--- examples/async/promises.js
+++ examples/async/promises.js
@@ -1,13 +1,9 @@
-const fs = require("fs");
-const { promisify } = require("util");
+const fs = require("fs").promises;
 
-const readFile = promisify(fs.readFile);
-const readdir = promisify(fs.readdir);
-
-readdir(".")
+fs.readdir(".")
   .then((files) => {
     console.log("files: ", files);
-    return readFile(files[0], { encoding: "utf-8" });
+    return fs.readFile(files[0], { encoding: "utf-8" });
   })
   .then((content) => {
     console.log("content: ", content);

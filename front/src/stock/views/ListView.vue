8b7b1f1e5bf4f7d400cd52b05e36b62b2670f1b1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-01 10:26:40 +0200

    dev: added reactive variable

--- front/src/stock/views/ListView.vue
+++ front/src/stock/views/ListView.vue
@@ -1,4 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Article } from '../interfaces/Article'
+
+const articles = ref<Article[]>([
+  {
+    id: 'a1',
+    name: 'Tournevis',
+    price: 3.99,
+    qty: 345
+  },
+  {
+    id: 'a2',
+    name: 'Pelle',
+    price: 5,
+    qty: 12
+  }
+])
+
+setTimeout(() => {
+  articles.value.push({
+    id: 'a3',
+    name: 'xxx',
+    price: 5,
+    qty: 12
+  })
+}, 2000)
+</script>
 
 <template>
   <main>
@@ -24,25 +51,10 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td class="name">Tournevis</td>
-            <td class="price text-right">3.45 €</td>
-            <td class="qty text-right">123</td>
-          </tr>
-          <tr>
-            <td class="name">Pelle</td>
-            <td class="price text-right">13.45 €</td>
-            <td class="qty text-right">6</td>
-          </tr>
-          <tr class="selected">
-            <td class="name">Arrosoir</td>
-            <td class="price text-right">5.00 €</td>
-            <td class="qty text-right">12</td>
-          </tr>
-          <tr>
-            <td class="name">Marteau</td>
-            <td class="price text-right">34.00 €</td>
-            <td class="qty text-right">8</td>
+          <tr v-for="article in articles" :key="article.id">
+            <td class="name">{{ article.name }}</td>
+            <td class="price text-right">{{ article.price }} €</td>
+            <td class="qty text-right">{{ article.qty }}</td>
           </tr>
         </tbody>
       </table>

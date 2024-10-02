029a461f96cf9dfb57ea88a4472bb748526a8fb0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-02 14:54:59 +0200

    dev: added declaration

--- front/env.d.ts
+++ front/env.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="vite/client" />
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import AsyncButton from '@/plugins/widgets/AsyncButton.vue'
 
 declare module 'vue' {
   export interface GlobalComponents {
     FontAwesomeIcon: typeof FontAwesomeIcon
+    AsyncButton: typeof AsyncButton
   }
 }

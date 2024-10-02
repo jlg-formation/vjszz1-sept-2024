ec2839a1c67fc7a25cceb496e95062e51a6be051
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-02 15:58:17 +0200

    dev: not found component

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,6 +1,7 @@
 import { stockRoutes } from '@/stock/stock.routes'
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,7 +26,7 @@ const router = createRouter({
       }
     },
     ...stockRoutes,
-    { path: '/:catchAll(.*)', redirect: '/' }
+    { path: '/:catchAll(.*)', name: 'legal', component: NotFound }
   ]
 })
 
---
+++ front/src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <h1>Pas trouvée 😥</h1>
+    <RouterLink to="/">Accueil</RouterLink>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: column;
+}
+</style>

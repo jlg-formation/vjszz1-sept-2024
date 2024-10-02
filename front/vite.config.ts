dac06832ced3297207f71b06a7de160eb66716ad
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-02 15:22:25 +0200

    dev: remove console log

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,13 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+import removeConsole from 'vite-plugin-remove-console'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueJsx(), vueDevTools(), removeConsole()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

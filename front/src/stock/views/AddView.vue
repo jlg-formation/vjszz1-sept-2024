28e4c51511ae434ee060acd244d4983d9f791548
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-02 09:47:49 +0200

    dev: added v-model

--- front/src/stock/views/AddView.vue
+++ front/src/stock/views/AddView.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+import { reactive } from 'vue'
+import type { NewArticle } from '../interfaces/Article'
+
 const handleSubmit = () => {
   console.log('submit')
 }
+
+const newArticle = reactive<NewArticle>({
+  name: 'Truc',
+  price: 0,
+  qty: 0
+})
 </script>
 
 <template>
@@ -10,15 +19,15 @@ const handleSubmit = () => {
     <form @submit.prevent="handleSubmit">
       <label>
         <span>Nom</span>
-        <input type="text" />
+        <input type="text" v-model="newArticle.name" />
       </label>
       <label>
         <span>Prix</span>
-        <input type="number" />
+        <input type="number" v-model="newArticle.price" />
       </label>
       <label>
         <span>Quantité</span>
-        <input type="number" />
+        <input type="number" v-model="newArticle.qty" />
       </label>
       <button class="primary">
         <FontAwesomeIcon icon="fa-plus" />

0b230edbddf21859c76e76c541807958faf4965b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-10-02 15:42:34 +0200

    dev: BFF

--- front/src/stock/apiClient.ts
+++ front/src/stock/apiClient.ts
@@ -1,7 +1,7 @@
 import { sleep } from '@/utils/sleep'
 import type { Article, NewArticle } from './interfaces/Article'
 
-const url = 'http://localhost:3000/api/articles'
+const url = '/api/articles'
 
 class API {
   async add(newArticle: NewArticle): Promise<void> {
